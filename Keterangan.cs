72a7f72caae04a0e62143f8a17b8c3994fed6b9e
Rizky Khapidsyah <rizkyhafitsyah@gmail.com>
2019-12-23 02:55:01 +0700

    Add project files.

---
+++ Keterangan.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassDanObject__CS
+{
+    class Keterangan
+    {
+        public void Nama(string N)
+        {
+            Console.WriteLine("Nama Anda Adalah : " + N);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ClassDanObject__CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String NamaAnda;
+            Keterangan Orang = new Keterangan();
+
+            Console.Write("Masukkan Nama Anda : "); NamaAnda = Console.ReadLine();
+
+            Orang.Nama(NamaAnda);
+
+            Console.ReadLine();
+        }
+    }
+}
